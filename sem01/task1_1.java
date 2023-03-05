8dc7e54f6b3b78a05f29f12ed34811349a86509a
Yakovlev Andrew <drozy@yandex.ru>
2023-03-05 21:19:36 +0300

    Добавлены решения 1 и 2 задач 1го семинара

--- sem01/task1_1.java
+++ sem01/task1_1.java
@@ -1,10 +1,35 @@
 package sem01;
 
+import java.util.Scanner;
+
 /*
  * Вычислить n-ое треугольноe число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
  */
 public class task1_1 {
+    private static int triangularNumber(int num) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            res += i;
+        }
+        return res;
+    }
+
+    private static int factorial(int num) {
+        int res = 1;
+        for (int i = 1; i <= num; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Задайте число n: ");
+        int number = scanner.nextInt();
+        int tr = triangularNumber(number);
+        System.out.printf("%d-ое треугольное число равно %d\n", number, tr);
+        int fac = factorial(number);
+        System.out.printf("%d! = %d\n", number, fac);
+        scanner.close();
     }
 }
--- sem01/task1_2.java
+++ sem01/task1_2.java
@@ -5,6 +5,18 @@ package sem01;
  */
 public class task1_2 {
     public static void main(String[] args) {
-        
+        StringBuilder output = new StringBuilder();
+        output.append("1 ");
+        for (int i = 2; i < 1000; i++) {
+            int count = 0;
+            for (int j = 2; j <= i && count < 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count < 2)
+                output.append(i + " ");
+        }
+        System.out.println(output);
     }
 }
